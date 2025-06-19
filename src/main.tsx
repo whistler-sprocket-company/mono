f754413fefb3fe8038f6e5c8ef51c62916cada15
Michael B <mjbradvica@gmail.com>
2025-06-19 12:12:46 -0400

    use styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/app";
+import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
