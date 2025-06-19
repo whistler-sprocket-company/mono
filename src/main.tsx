bd22939c52306be3d5a74cbb475ce8bb13391c9f
Michael B <mjbradvica@gmail.com>
2025-06-19 12:19:53 -0400

    add linting

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/app";
 import "./styles/main.scss";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") ?? new Element()).render(
   <StrictMode>
     <App />
   </StrictMode>,
